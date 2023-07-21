479d7486a89efb39d957ffb866741f72d2a9a236
Annie-dev01 <annabeladaeze@gmail.com>
2023-07-21 11:28:36 +0000

    Added dist folder to .gitignore

--- dist/bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const http = require('http');
-const app = require('../app');
-const server = http.createServer(app);
-const PORT = process.env.PORT || 4004;
-server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
